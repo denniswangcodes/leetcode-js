efcfe632fd77201a9f738fdc60bc8e8f8cc60f89
Dennis Wang <denniswang.pro@gmail.com>
2025-08-28 23:38:51 -0700

    Missing Number

---
+++ Easy/missingNumber.js
@@ -0,0 +1,6 @@
+var missingNumber = function(nums) {
+    let n = nums.length;
+    let expected = n * (n + 1) / 2;
+    let actual = nums.reduce((a, b) => a + b, 0);
+    return expected - actual;
+};
